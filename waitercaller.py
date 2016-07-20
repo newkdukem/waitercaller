5538fc708fac0c2b1a742b0f0182688dbaf2d460
Nikola Demerdjiev <newkdukem@gmail.com>
2016-07-20 19:07:48 +0200

    Created mock user db

--- waitercaller.py
+++ waitercaller.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask import render_template
 from flask.ext.login import LoginManager
 from flask.ext.login import login_required
+from flask.ext.login import login_user
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
+app.secret_key = 'zV+ftkCCiWBYvFO/WHkj7UiRhmIvRZi2PFk4xw04SkRLzFkYm2ZSDDEA6E/nAXuzwKU+zYN8/Exx2UcdrRY65VOKgG+e73qbCWM'
 login_manager = LoginManager(app)
+DB = DBHelper()
 
 @app.route("/")
 def home():
