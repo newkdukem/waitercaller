b09151fe3b3e4c12f827fa1c4943859fbc1bd4f9
Nikola Demerdjiev <newkdukem@gmail.com>
2016-07-20 18:53:06 +0200

    user class added

--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask.ext.login import LoginManager
+from flask.ext.login import login_required
+
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in"
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
