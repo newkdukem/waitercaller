9dc569493f6c0d1b475540f92368de01703b4dc2
Nikola Demerdjiev <newkdukem@gmail.com>
2016-07-26 22:24:02 +0200

    Chapter 10, pg 187

--- passwordhelper.py
+++ passwordhelper.py
@@ -2,11 +2,12 @@ import hashlib
 import os
 import base64
 
-class PosswardHelper:
+class PasswordHelper:
     def get_hash(self, plain):
         return hashlib.sha512(plain).hexdigest()
 
     def get_salt(self):
         return base64.b64encode(os.urandom(20))
+
     def validate_password(self, plain, salt, expected):
         return self.get_hash(plain + salt) == expected
--- waitercaller.py
+++ waitercaller.py
@@ -4,16 +4,23 @@ from flask_login import LoginManager
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
+from flask_login import current_user
 from mockdbhelper import MockDBHelper as DBHelper
 from user import User
 from flask import redirect
 from flask import url_for
 from flask import request
+from passwordhelper import PasswordHelper
+from bitlyhelper import BitlyHelper
+import config
+import datetime
 
 app = Flask(__name__)
 app.secret_key = 'zV+ftkCCiWBYvFO/WHkj7UiRhmIvRZi2PFk4xw04SkRLzFkYm2ZSDDEA6E/nAXuzwKU+zYN8/Exx2UcdrRY65VOKgG+e73qbCWM'
 login_manager = LoginManager(app)
 DB = DBHelper()
+PH = PasswordHelper()
+BH = BitlyHelper()
 
 @app.route("/")
 def home():
@@ -22,18 +29,41 @@ def home():
 @app.route("/account")
 @login_required
 def account():
-    return "You are loged in"
+    tables = DB.get_tables(current_user.get_id())
+    return render_template("account.html", tables=tables)
+
+@app.route("/account/createtable", methods=["POST"])
+@login_required
+def account_createtable():
+    tablename = request.form.get("tablenumber")
+    tableid = DB.add_table(tablename, current_user.get_id())
+    new_url = BH.shorten_url(config.base_url + "newrequest/" + tableid)
+    DB.update_table(tableid, new_url)
+    return redirect(url_for('account'))
+
+@app.route("/account/deletetable")
+@login_required
+def account_deletetable():
+    tableid = request.args.get("tableid")
+    DB.delete_table(tableid)
+    return redirect(url_for('account'))
 
 @app.route("/login", methods=["POST"])
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
         user = User(email)
         login_user(user, remember=True)
         return redirect(url_for('account'))
     return home()
+
+@app.rout("/newrequest/<tid>")
+def new_request(tid):
+    DB.add_request(tid.datetime.datetime.now())
+    return "Your request has been logged and a waiter will be with you shortly"
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -45,6 +75,27 @@ def load_user(user_id):
 def logout():
     logout_user()
     return redirect(url_for("home"))
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
